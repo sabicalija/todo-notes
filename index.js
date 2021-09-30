1a44eead9ba647def1057c26d510fd40cb233440
Alija Sabic <sabic.alija@gmail.com>
2021-10-01 00:51:59 +0200

    Improve UX: add todos on keyboard 'Enter'

--- index.js
+++ index.js
@@ -2,7 +2,20 @@
 const btnAddToDo = document.getElementById("add-todo");
 btnAddToDo.addEventListener("click", handleClick);
 
+const input = document.getElementById("todo-note");
+input.addEventListener("keydown", handleKeyDown);
+
 function handleClick(event) {
+  addToDo();
+}
+
+function handleKeyDown(event) {
+  if (event.key === "Enter") {
+    addToDo();
+  }
+}
+
+function addToDo() {
   // https://developer.mozilla.org/en-US/docs/Web/API/HTMLUListElement
   const list = window.document.getElementById("todo-list");
   // https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement
