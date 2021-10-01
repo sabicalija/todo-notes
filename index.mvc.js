fe28df21cd70cd3a6f9049ce0417b1f8b3cc4eab
Alija Sabic <sabic.alija@gmail.com>
2021-10-01 02:12:45 +0200

    Reimplement basic functionality with MVC pattern

---
+++ index.mvc.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const btnAddToDo = document.getElementById("add-todo");
+  btnAddToDo.addEventListener("click", handleClick);
+
+  const input = document.getElementById("todo-note");
+  input.addEventListener("keydown", handleKeyDown);
+
+  updateUI();
+});
+
+// Model
+const todos = [
+  // "ToDo 1",
+  // "Todo 2"
+];
+
+// View
+function clearInput() {
+  const input = document.getElementById("todo-note");
+  input.value = "";
+}
+
+function updateUI() {
+  updateToDoList();
+}
+
+function updateToDoList() {
+  const list = document.getElementById("todo-list");
+
+  // Clear list
+  while (list.firstChild) {
+    list.firstChild.remove();
+  }
+
+  // Build list
+  todos.map((todo) => buildListItem(todo)).forEach((item) => list.appendChild(item));
+}
+
+function buildListItem(todo) {
+  const item = document.createElement("li");
+  item.textContent = todo;
+  return item;
+}
+
+function getInput() {
+  const input = document.getElementById("todo-note");
+  return input.value;
+}
+
+function handleClick(event) {
+  const text = getInput();
+  addToDo(text);
+  clearInput();
+  updateUI();
+}
+
+function handleKeyDown(event) {
+  if (event.key === "Enter") {
+    const text = getInput();
+    addToDo(text);
+    clearInput();
+    updateUI();
+  }
+}
+
+// Controller
+function addToDo(text) {
+  todos.push(text);
+}
