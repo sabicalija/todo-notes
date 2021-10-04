6dc3f37f20b96f238fdb60dfc1e5ff5f802b3595
Alija Sabic <sabic.alija@gmail.com>
2021-10-05 01:45:24 +0200

    Extend data model according to new structure example, update build function to generate list items as required

--- index.mvc.js
+++ index.mvc.js
@@ -1,18 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const btnAddToDo = document.getElementById("add");
-  btnAddToDo.addEventListener("click", handleClick);
-
-  const input = document.getElementById("text");
-  input.addEventListener("keydown", handleKeyDown);
-
-  // updateUI();
+  const save = document.getElementById("save");
+  save.addEventListener("click", handleClickSave);
+  updateUI();
 });
 
 // Model
 const todos = [
-  // { id: "676c9ba771", text: "ToDo 1" },
-  // { id: "dc19d1538f", text: "ToDo 2" },
-  // { id: "fd8c75b4fb", text: "ToDo 2" },
+  // { id: "676c9ba771", title: "Title 1", text: "ToDo 1", color: "#fff740" },
+  // { id: "dc19d1538f", title: "Title 2", text: "ToDo 2", color: "#fff740" },
+  // { id: "fd8c75b4fb", title: "Title 2", text: "ToDo 2", color: "#fff740" },
 ];
 
 const colors = ["#fff740", "#feff9c", "#7afcff", "#ff65a3", "#ff7eb9"];
@@ -23,7 +19,7 @@ function updateUI() {
 }
 
 function updateToDoList() {
-  const list = document.getElementById("todo-list");
+  const list = document.getElementById("list");
 
   // Clear list
   while (list.firstChild) {
@@ -37,72 +33,90 @@ function updateToDoList() {
 
 function buildListItem(todo) {
   const item = document.createElement("li");
-  item.classList.add("todo-item");
+  item.classList.add("todo__list__item", "controlable", "material");
 
-  const btnDelete = document.createElement("button");
-  btnDelete.textContent = "x";
-  btnDelete.addEventListener("click", () => {
+  const title = document.createElement("div");
+  title.classList.add("todo__list__item__title");
+  title.textContent = todo.title;
+
+  const text = document.createElement("div");
+  text.classList.add("todo__list__item__text");
+  text.textContent = todo.text;
+
+  const control = document.createElement("div");
+  control.classList.add("todo__list__item__control");
+
+  const buttonDelete = document.createElement("button");
+  buttonDelete.title = "Delete note";
+  buttonDelete.classList.add("todo__list__item__control--delete", "btn", "btn-size-1");
+
+  const buttonDeleteIcon = document.createElement("i");
+  buttonDeleteIcon.classList.add("fas", "fa-trash");
+  buttonDelete.appendChild(buttonDeleteIcon);
+  buttonDelete.addEventListener("click", () => {
     removeToDo(todo.id);
     updateUI();
   });
-  const divTitle = document.createElement("div");
-  const spanTitle = document.createElement("span");
-  spanTitle.textContent = todo.title;
-  divTitle.appendChild(spanTitle);
 
-  const divText = document.createElement("div");
-  const spanText = document.createElement("span");
-  spanText.textContent = todo.text;
-  divText.appendChild(spanText);
+  const buttonEdit = document.createElement("button");
+  buttonEdit.title = "Edit note";
+  buttonEdit.classList.add("todo__list__item__control--edit", "btn", "btn-size-1");
 
-  item.appendChild(btnDelete);
-  item.appendChild(divTitle);
-  item.appendChild(divText);
+  const buttonEditIcon = document.createElement("i");
+  buttonEditIcon.classList.add("fas", "fa-pen");
+  buttonEdit.appendChild(buttonEditIcon);
+
+  const buttonColor = document.createElement("button");
+  buttonColor.title = "Change color";
+  buttonColor.classList.add("todo__list__item__control--color", "btn", "btn-size-1");
+
+  const buttonColorIcon = document.createElement("i");
+  buttonColorIcon.classList.add("fas", "fa-palette");
+  buttonColor.appendChild(buttonColorIcon);
+
+  control.appendChild(buttonDelete);
+  control.appendChild(buttonEdit);
+  control.appendChild(buttonColor);
+
+  item.appendChild(title);
+  item.appendChild(text);
+  item.appendChild(control);
 
   return item;
 }
 
 function getInput() {
-  const inputTitle = document.getElementById("todo-note-title");
-  const inputText = document.getElementById("todo-note");
-  return [inputTitle.value, inputText.value];
+  const title = document.getElementById("title");
+  const text = document.getElementById("text");
+  const color = { value: "#fff740" }; // document.getElementById("color");
+  return [title.value, text.value, color.value];
 }
 
 function clearInput() {
-  const inputTitle = document.getElementById("todo-note-title");
-  const inputText = document.getElementById("todo-note");
-  inputTitle.value = inputText.value = "";
+  const title = document.getElementById("title");
+  const text = document.getElementById("text");
+  title.value = text.value = "";
 }
 
-function handleClick(event) {
-  const [title, text] = getInput();
+function handleClickSave(event) {
+  const [title, text, color] = getInput();
   if (text !== "") {
-    addToDo(title, text);
+    addToDo(title, text, color);
     clearInput();
     updateUI();
   }
 }
 
-function handleKeyDown(event) {
-  if (event.key === "Enter") {
-    const [title, text] = getInput();
-    if (text !== "") {
-      addToDo(title, text);
-      clearInput();
-      updateUI();
-    }
-  }
-}
-
 // Controller
-function addToDo(title, text) {
-  const id = CryptoJS.SHA256(title + text + new Date())
+function addToDo(title, text, color) {
+  const id = CryptoJS.SHA256(title + text + color + new Date())
     .toString()
     .substring(0, 10);
   const todo = {
     id: id,
     title: title,
     text: text,
+    color: color,
   };
   todos.push(todo);
 }
